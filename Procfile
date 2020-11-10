3d1977d8f60d6794840093a95e427d97d253ea27
alanmsant2 <alanmsant@gmail.com>
2020-11-10 09:46:35 -0300

    criado env para secretkey e add release em procfile

--- Procfile
+++ Procfile
@@ -1,1 +1,4 @@
-web: gunicorn sapo_alimenticio.wsgi
+release: python manage.py makemigrations --no-input
+release: python manage.py migrate --no-input
+
+web: gunicorn api.wsgi
--- api/settings.py
+++ api/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 import django_heroku
 from pathlib import Path
 
@@ -22,10 +22,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'atcoag0q3w45p#uc4eeh!u9w)m3t11bqo^bq2hoc&dy9=z-2tx'
+# SECRET_KEY = 'atcoag0q3w45p#uc4eeh!u9w)m3t11bqo^bq2hoc&dy9=z-2tx'
+SECRET_KEY = os.environ('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
