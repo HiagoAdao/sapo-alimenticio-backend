a53cdb8f587d5308d43b481bf1266fc296b3dced
alanmsant2 <alanmsant@gmail.com>
2020-11-10 15:28:06 -0300

    definido DATABASES remoto heroku

--- api/settings.py
+++ api/settings.py
@@ -84,6 +84,15 @@ DATABASES = {
     }
 
     # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'NAME': 'postgres',
+    #     'USER': 'postgres',
+    #     'PASSWORD': 'postgres',
+    #     'HOST': 'localhost',
+    #     'PORT': '5432',
+    # }
+
+    # 'default': {
     #     'ENGINE': 'django.db.backends.sqlite3',
     #     'NAME': BASE_DIR / 'db.sqlite3',
     # }
